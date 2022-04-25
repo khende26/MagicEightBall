a2df5773bed290adf229ea315e08e0b3b0bde893
Karri Wright <karriwright@Karris-MacBook-Pro.local>
2022-04-25 16:04:12 -0400

    Add BlackJackgame

---
+++ src/main/java/com/detroitlabs/magiceightball/controller/BlackJackController.java
@@ -0,0 +1,2 @@
+package com.detroitlabs.magiceightball.controller;public class BlackJackController {
+}
---
+++ src/main/java/com/detroitlabs/magiceightball/model/Magic8BallResponses.java
@@ -0,0 +1,2 @@
+package com.detroitlabs.magiceightball.model;public class Magic8BallResponses {
+}
